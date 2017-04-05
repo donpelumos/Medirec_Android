83e164f4ce35d8947557e2efee968db5e572e95b
Don Pelumos <donpelumos@gmail.com>
2017-04-05 22:54:27 +0100

    fourth

---
+++ Medirec/app/src/main/java/com/jempton/medirec/ActivityFeedDoctor.java
@@ -0,0 +1,18 @@
+package com.jempton.medirec;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by BALE on 05/04/2017.
+ */
+
+public class ActivityFeedDoctor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feed_doctor);
+    }
+}
